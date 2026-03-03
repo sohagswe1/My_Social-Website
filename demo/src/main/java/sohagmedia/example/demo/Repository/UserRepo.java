d68ae4c9423e55fef5b3093a4f3a0478f642bcda
sohagswe <sohagpage@gmail.com>
2026-03-03 23:49:09 +0600

    create  UserDetailsService

--- demo/src/main/java/sohagmedia/example/demo/Repository/UserRepo.java
+++ demo/src/main/java/sohagmedia/example/demo/Repository/UserRepo.java
@@ -2,10 +2,12 @@ package sohagmedia.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
 import sohagmedia.example.demo.Entity.User;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User >findByEmail(String email);
 
 }
---
+++ demo/src/main/java/sohagmedia/example/demo/Service/Myuserdetailsservice.java
@@ -0,0 +1,32 @@
+package sohagmedia.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sohagmedia.example.demo.Entity.User;
+import sohagmedia.example.demo.Repository.UserRepo;
+import sohagmedia.example.demo.Repository.UserRepo;
+
+import java.util.Optional;
+
+@Service
+public class Myuserdetailsservice implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
