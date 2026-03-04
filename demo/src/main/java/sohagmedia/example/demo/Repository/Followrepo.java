859fa2cc0de6f2ab966426cd744a684beaf6f6c3
sohag <sohagpage@gmail.com>
2026-03-04 13:09:02 +0600

    some update

--- demo/src/main/java/sohagmedia/example/demo/Repository/Followrepo.java
+++ demo/src/main/java/sohagmedia/example/demo/Repository/Followrepo.java
@@ -7,5 +7,6 @@ import sohagmedia.example.demo.Entity.Follow;
 @Repository
 public interface Followrepo extends JpaRepository<Follow, Long> {
     //follow sohag...........................follow....
+    //ddd
 
 }
